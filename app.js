cd9041fd4081fbffa3f9caeb31a727001d53ab52
4cruzeta <4cruzetam@gmail.com>
2020-02-08 19:34:53 -0300

    Funcionalidade com JS

---
+++ app.js
@@ -0,0 +1,8 @@
+const itemInput = document.querySelector('#input-item');
+const valorInput = document.querySelector('#input-valor');
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmlBtn = document.querySelector('#btn-confirm');
+
+confirmlBtn.addEventListener('click', () => {
+    console.log('Funcionou !!!')
+});
